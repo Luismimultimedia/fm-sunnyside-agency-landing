80248aa53a6adb743e8b34f778f7fef9eda8557b
Luis Miguel Gonzalez <luismimultimedia@gmail.com>
2022-07-26 20:27:23 -0500

    feat: create new content in the landing

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
+import '../styles/global.scss'
+
+let isMenuOpen = false;
+const menuButton = document.getElementById("menu-btn");
+const menu = document.getElementsByClassName("header__navigation");
+const setMenuStatus = () => menu[0].style.display = isMenuOpen ? "flex" : "none";
+
+setMenuStatus();
+
+menuButton.addEventListener("click", () => {
+    isMenuOpen = !isMenuOpen;
+    setMenuStatus();
+});
+
 
